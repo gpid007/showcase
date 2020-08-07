6bacb92a9df0e5c02744b1e185096e9c9a88142c
Gregor-Patrick Heine <heine.gregor@gmail.com>
2020-08-07 22:30:56 +0200

    parameters

--- bash.d/rename.sh
+++ bash.d/rename.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
-echo "$0"
+# GLOBAL DEFAULTS
+findDir='.'
+prefix='audiofile_'
+suffix='.wav'
+creation=''
+counter=0
 
-exit 0
+# PARAMETERS
+while ("$#"); do
+    case "$1" in
+        --dir|-d)   findDir="$1"                                ;;
+        --suf|-s)   suffix="$1"                                 ;;
+        --pre|-p)   prefix="$1"                                 ;;
+        *)          echo "Please enter valid parameter flag."   ;;
+    esac
+    shift
+done
+
+
+if [ ! -d "$findDir" ]; then
+    echo "Cannot stat '$findDir': No such directory"
+    return 2
+fi
+
+# find                    \
+#     "$findDir"          \
+#     -type f             \
+#     -iname "*$suffix"
+# stat -c %y $filePath | awk '{print $1}'
+# printf "%03d\n" $counter
+# (( counter++ ))
