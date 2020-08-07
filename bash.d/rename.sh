5401643ee70fc4b8012ec6deb9758ec0f81849e3
Gregor-Patrick Heine <heine.gregor@gmail.com>
2020-08-08 00:24:51 +0200

    rename.sh done

--- bash.d/rename.sh
+++ bash.d/rename.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 
 # GLOBALS
-findDir='.'
-prefix='audiofile_'
-suffix='.wav'
-sortType='g'
-creation=''
-fileList=''
 counter=0
+findDir='.'
+prefix='audiofile'
+sortType='g'
+suffix='.wav'
+helpMsg='
+INPUT PARAMETERS
+    --dir | -d  ==  target directory    eg.     -d .
+    --suf | -s  ==  target suffix       eg.     -s wav
+    --pre | -p  ==  target prefix       eg.     -p audiofile
+    --rev | -r  ==  reverse sorting     eg.     -r [leave empty]
+EXAMPLE
+    ./rename.sh -d pathToDir -r -p ABBA
+'
 
 # PARAMETERS
 while (( "$#" )); do
     case "$1" in
-        --dir|-d)   shift; findDir="$1"                     ;;
-        --suf|-s)   shift; suffix="$1"                      ;;
-        --pre|-p)   shift; prefix="$1"                      ;;
-        --rev|-r)   sortType='gr'                           ;;
-        *)          echo "Invalid parameter $1"; exit 2     ;;
+        --dir|-d)   shift; findDir="$1"                                       ;;
+        --suf|-s)   shift; suffix="$1"                                        ;;
+        --pre|-p)   shift; prefix="$1"                                        ;;
+        --rev|-r)   sortType='gr'                                             ;;
+        --help|-h)  echo -e "$helpMsg"; exit 2                                ;;
+        *)          echo -e "Invalid parameter '$1'\nEnter --help|-h"; exit 2 ;;
     esac
     shift
 done
@@ -25,23 +33,24 @@ done
 if [ ! -d "$findDir" ]; then
     echo "Cannot stat '$findDir': No such directory"
     exit 2
+else
+    findDir=$(realpath "$findDir")
 fi
 
-fileList=$(                         \
+while read line; do
+    (( counter++ ))
+    arr=($line)
+    newName=$(printf "%s_%s_%03d%s\n" $prefix ${arr[1]} $counter $suffix)
+    echo ${arr[0]} $findDir/$newName
+done < <(
     find                            \
         "$findDir"                  \
         -type f                     \
         -iname "*$suffix"           \
         -printf "%p %CY-%Cm-%Cd\n"  \
-    | sort -"$sortType"
+    | sort -"$sortType"             \
 )
 
-for file in $fileList; do
-    echo "$"
-done
-
-# stat -c %y $fileList | awk '{print $1}'
-# printf "%03d\n" $counter
-# (( counter++ ))
-
-echo SUCCESS
+if [ $counter -lt 1 ]; then
+    echo "No '*.$suffix' files in '$findDir'"
+fi
