562b57a86228daf8fd5a2991d82028fd1e2b7a00
Gregor-Patrick Heine <heine.gregor@gmail.com>
2020-08-12 23:45:27 +0200

    spaces in file name handled

--- bash.d/rename.sh
+++ bash.d/rename.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 # GLOBALS
-arr=()
-counter=0
 findDir='.'
-newName=''
+printfStr=''
 prefix='audiofile'
 sortType='g'
 suffix='.wav'
@@ -21,12 +19,12 @@ EXAMPLE
 # PARAMETERS
 while (( "$#" )); do
     case "$1" in
-        --dir|-d)   shift; findDir="$1"                                       ;;
-        --suf|-s)   shift; suffix="$1"                                        ;;
-        --pre|-p)   shift; prefix="$1"                                        ;;
-        --rev|-r)   sortType='gr'                                             ;;
-        --help|-h)  echo -e "$helpMsg"; exit 2                                ;;
-        *)          echo -e "Invalid parameter '$1'\nEnter --help|-h"; exit 2 ;;
+        --dir|-d)   shift; findDir="$1"                                 ;;
+        --suf|-s)   shift; suffix="$1"                                  ;;
+        --pre|-p)   shift; prefix="$1"                                  ;;
+        --rev|-r)   sortType='gr'                                       ;;
+        --help|-h)  echo -e "$helpMsg";                      exit 2     ;;
+        *)          echo -e "'$1' invalid\nEnter --help|-h"; exit 2     ;;
     esac
     shift
 done
@@ -36,33 +34,18 @@ if [ ! -d "$findDir" ]; then
     echo "Cannot stat '$findDir': No such directory"
     exit 2
 else
-    findDir=$(realpath "$findDir")
+    cd "$findDir"
+    printfStr="'%s' ${findDir}/${prefix}_%s_%03d${suffix}\n"
 fi
 
-# while read line; do
-#     (( counter++ ))
-#     arr=($line)
-#     newName=$(printf "%s_%s_%03d%s\n" $prefix ${arr[1]} $counter $suffix)
-#     echo ${arr[0]} $findDir/$newName
-# done < <(
-#     find                            \
-#         "$findDir"                  \
-#         -type f                     \
-#         -iname "*$suffix"           \
-#         -printf "%p %CY-%Cm-%Cd\n"  \
-#     | sort -"$sortType"             \
-# )
+find "$findDir" -type f -name "*$suffix" -printf "%p;%CY-%Cm-%Cd\n"     \
+| sort -"$sortType"                                                     \
+| awk -F';' '{print $0 ";" NR}'                                         \
+| awk -F';' "{printf \"$printfStr\", \$1,\$2,\$3}"                      \
+| while read line; do
+    echo "mv $line"
+    eval "mv $line"
+done
 
-findList=$(                             \
-    find                                \
-        "$findDir"                      \
-        -type f                         \
-        -iname "*$suffix"               \
-        -printf "%p;%CY-%Cm-%Cd\n"      \
-    | sort -g                           \
-    | awk -F';' '{print $0 ";" NR}'     \
-)
-
-# if [ $counter -lt 1 ]; then
-#     echo "No '*.$suffix' files in '$findDir'"
-# fi
+# CREATE DUMMY FILES
+# touch 'a a.wav' b.wav c.wav 'd d.wav'
