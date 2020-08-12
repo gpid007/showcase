8c5b35a223f87b28cce9092dd67b619c0ac8f35d
Gregor-Patrick Heine <heine.gregor@gmail.com>
2020-08-12 23:56:16 +0200

    help improved and case more legible

--- bash.d/rename.sh
+++ bash.d/rename.sh
@@ -7,24 +7,24 @@ prefix='audiofile'
 sortType='g'
 suffix='.wav'
 helpMsg='
-INPUT PARAMETERS
-    --dir | -d  ==  target directory    eg.     -d .
-    --suf | -s  ==  target suffix       eg.     -s wav
-    --pre | -p  ==  target prefix       eg.     -p audiofile
-    --rev | -r  ==  reverse sorting     eg.     -r [leave empty]
-EXAMPLE
-    ./rename.sh -d pathToDir -r -p ABBA
+    INPUT PARAMETERS
+        --dir, -d   directory-path           -d "."
+        --suf, -s   suffix (file-extension)  -s "wav"
+        --pre, -p   prefix                   -p "audiofile"
+        --rev, -r   reverse-sort             -r [blank]
+    EXAMPLE
+        ./rename.sh -d pathToDir -r -p ABBA
 '
 
 # PARAMETERS
 while (( "$#" )); do
     case "$1" in
-        --dir|-d)   shift; findDir="$1"                                 ;;
-        --suf|-s)   shift; suffix="$1"                                  ;;
-        --pre|-p)   shift; prefix="$1"                                  ;;
-        --rev|-r)   sortType='gr'                                       ;;
-        --help|-h)  echo -e "$helpMsg";                      exit 2     ;;
-        *)          echo -e "'$1' invalid\nEnter --help|-h"; exit 2     ;;
+        --dir|-d)   shift;  findDir="$1"                                ;;
+        --suf|-s)   shift;  suffix="$1"                                 ;;
+        --pre|-p)   shift;  prefix="$1"                                 ;;
+        --rev|-r)           sortType='gr'                               ;;
+        --help|-h)  echo -e "$helpMsg"                      ; exit 2    ;;
+        *)          echo -e "'$1' invalid\nEnter --help|-h" ; exit 2    ;;
     esac
     shift
 done
@@ -44,7 +44,7 @@ find "$findDir" -type f -name "*$suffix" -printf "%p;%CY-%Cm-%Cd\n"     \
 | awk -F';' "{printf \"$printfStr\", \$1,\$2,\$3}"                      \
 | while read line; do
     echo "mv $line"
-    eval "mv $line"
+    # eval "mv $line"
 done
 
 # CREATE DUMMY FILES
